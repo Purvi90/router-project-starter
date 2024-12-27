8d6e84dc8d2d61f38638f726975d1be56333608f
Purvi90 <purvisharma1239@gmail.com>
2024-12-27 18:51:16 +0530

    upload file

---
+++ src/components/LoginForm.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <form>
+        <label>
+            <p>
+                Email Address <sup>*</sup>
+            </p>
+            <input
+            
+            />
+        </label>
+    </form>
+  )
+}
+
+export default LoginForm
